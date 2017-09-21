003f6e25f9ea1e590f38b983ff8daf108cc5c372
mynomoto <mynomoto@gmail.com>
2017-09-21 13:26:08 -0300

    WIP Implement parser

--- src/Main.elm
+++ src/Main.elm
@@ -6,17 +6,21 @@ import Html.Attributes exposing (id, src)
 import MainCss
 import Assets exposing (url, hero)
 
-
 type Command = TurnRight | TurnLeft | Move
 type Direction = North | East | South | West
-type Status = Ok | Kabum
+type Status = Working | Kabum
 type alias Position = { x: Int, y: Int }
 
 type alias Dimension = { x: Int, y: Int }
 
 type alias Rover = { position: Position
                    , direction: Direction
-                   , status: Status}
+                   , status: Status
+                   }
+
+type alias Mars = { rovers: List Rover
+                  , dimension: Dimension
+                  }
 
 turnRight : Direction -> Direction
 turnRight direction = case direction of
@@ -67,13 +71,73 @@ move rover dimension = case rover.direction of
 runCommand : Rover -> Dimension -> Command -> Rover
 runCommand rover dimension command = case rover.status of
     Kabum -> rover
-    Ok -> case command of
+    Working -> case command of
    TurnRight -> { rover | direction = turnRight rover.direction }
    TurnLeft -> { rover | direction = turnLeft rover.direction }
    Move -> move rover dimension
 
+parseCharCommands : List Char -> List Command
+parseCharCommands input = case input of
+  [] -> []
+  'M' :: rest -> Move :: parseCharCommands rest
+  'R' :: rest -> TurnRight :: parseCharCommands rest
+  'L' :: rest -> TurnLeft :: parseCharCommands rest
+  _ :: rest -> parseCharCommands rest
+
+parseCommand : String -> List Command
+parseCommand input = input |> String.trim |> String.toUpper |> String.toList |> parseCharCommands
+
+parseDimensionInput : List String -> Maybe Dimension
+parseDimensionInput input = case input of
+   x :: y :: [] -> case String.toInt x of
+      Err _ -> Nothing
+      Ok xInt -> case String.toInt y of
+         Err _ -> Nothing
+         Ok yInt -> Just { x = xInt
+                         , y = yInt
+                         }
+   _ -> Nothing
+
+parseDimension : String -> Maybe Dimension
+parseDimension input = input |> String.trim |> String.words |> parseDimensionInput
+
+parseProbeInput : List String -> Maybe Rover
+parseProbeInput input = case input of
+   x :: y :: d :: [] -> case parseDimensionInput [x, y] of
+      Nothing -> Nothing
+      Just position -> case String.toUpper d of
+         "N" -> Just { position = position
+                     , direction = North
+                     , status = Working
+                     }
+         "E" -> Just { position = position
+                     , direction = East
+                     , status = Working
+                     }
+         "S" -> Just { position = position
+                     , direction = South
+                     , status = Working
+                     }
+         "W" -> Just { position = position
+                     , direction = West
+                     , status = Working
+                     }
+         _ -> Nothing
+
+   _ -> Nothing
+
+parseProbe : String -> Maybe Rover
+parseProbe input = input |> String.trim |> String.words |> parseProbeInput
+
 { id, class, classList } =
     withNamespace "main"
+
+-- Continue from here
+parseMars : List String -> Maybe Mars
+parseMars input = Nothing
+
+parseTextArea : String -> Maybe Mars
+parseTextArea input = input |> String.lines |> List.map String.trim |> List.filter String.isEmpty |> parseMars
 
 main : Html a
 main =
